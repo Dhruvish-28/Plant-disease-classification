3c9454fc67e26a6f561aab97971cb536791648e5
Dhruvish-28 <dhruvishparikh28@gmail.com>
2026-03-10 01:31:13 +0530

    page.tsx updated

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
     try {
       // Create FormData for file upload
       const formData = new FormData();
-      formData.append('image', file);
+      formData.append("file", file);
 
       // Call Flask backend
       const response = await fetch('https://dhruvish-28-plant-disease-classification.hf.space/classify', {
@@ -36,7 +36,7 @@ export default function Home() {
       }
 
       const data = await response.json();
-      setResults(data);
+      setResults(data.predictions);
     } catch (error) {
       setResults({
         error: error instanceof Error ? error.message : 'An error occurred',
